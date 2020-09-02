763057e7af7f686115708063b38957e663a1b48d
Elias <eli.bueno9@gmail.com>
2020-09-02 10:35:07 -0300

    Starting to develop the script to change the color of the div: inputs are already tracked

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const inputs = {
     topLeft: document.querySelector("#topLeft"),
     topRight: document.querySelector("#topRight"),
     bottomLeft: document.querySelector("#bottomLeft"),
-    bottomRight: document.querySelector("#bottomRight")
+    bottomRight: document.querySelector("#bottomRight"),
+    red: document.querySelector("#red"),
+    green: document.querySelector("#green"),
+    blue: document.querySelector("#blue"),
+    alpha: document.querySelector("#alpha")
 };
 
 //Take the div
@@ -27,11 +31,38 @@ function changeBottomRight(event) {
     theDiv.style.borderBottomRightRadius = `${event.target.value}px`;
 }
 
+function changeColor(redValue, greenValue, blueValue, alphaValue){
+    theDiv.style.backgroundColor = `rgba(${redValue}, ${greenValue}, ${blueValue}, ${alphaValue})`;
+}
+
+function storeRedValueColor(event){
+    console.log(event.target.value);
+}
+
+function storeGreenValueColor(event){
+    console.log(event.target.value);
+}
+
+function storeBlueValueColor(event){
+    console.log(event.target.value);
+}
+
+function storeAlphaValue(event){
+    console.log(event.target.value);
+}
+
 // Pass the event in the corresponding input
+//Border radius
 inputs.topLeft.addEventListener("input", changeTopLeft);
 inputs.topRight.addEventListener("input", changeTopRight);
 inputs.bottomLeft.addEventListener("input", changeBottomLeft);
 inputs.bottomRight.addEventListener("input", changeBottomRight);
+//Colors
+inputs.red.addEventListener("input", storeRedValueColor);
+inputs.green.addEventListener("input", storeGreenValueColor);
+inputs.blue.addEventListener("input", storeBlueValueColor);
+inputs.alpha.addEventListener("input", storeAlphaValue);
+
 
 function copyToClipboard(){
     // Create a text area element
