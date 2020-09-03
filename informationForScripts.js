cc8fde64cb19a2e53a96667466f7a4fee9cd5d60
Elias <eli.bueno9@gmail.com>
2020-09-03 11:20:45 -0300

    Starting to separate the script functionalities

---
+++ informationForScripts.js
@@ -0,0 +1,22 @@
+//Take the inputs
+const inputs = {
+    topLeft: document.querySelector("#topLeft"),
+    topRight: document.querySelector("#topRight"),
+    bottomLeft: document.querySelector("#bottomLeft"),
+    bottomRight: document.querySelector("#bottomRight"),
+    red: document.querySelector("#red"),
+    green: document.querySelector("#green"),
+    blue: document.querySelector("#blue"),
+    alpha: document.querySelector("#alpha")
+};
+
+//Colors
+const colors = {
+    red: 0,
+    green: 0,
+    blue: 0,
+    alpha: 1
+};
+
+//Take the div
+const theDiv = document.querySelector(".theDiv");
--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-//Take the inputs
-const inputs = {
-    topLeft: document.querySelector("#topLeft"),
-    topRight: document.querySelector("#topRight"),
-    bottomLeft: document.querySelector("#bottomLeft"),
-    bottomRight: document.querySelector("#bottomRight"),
-    red: document.querySelector("#red"),
-    green: document.querySelector("#green"),
-    blue: document.querySelector("#blue"),
-    alpha: document.querySelector("#alpha")
-};
-
-//Colors
-const colors = {
-    red: 0,
-    green: 0,
-    blue: 0,
-    alpha: 1
-};
-
-//Take the div
-const theDiv = document.querySelector(".theDiv");
-
-
 // Define the functions
 function changeTopLeft(event) {
     theDiv.style.borderTopLeftRadius = `${event.target.value}px`;
